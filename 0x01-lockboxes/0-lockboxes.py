cdd43dc9a8630fcb8d041707b75e3c758e04be20
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-14 17:06:38 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = set()
+
+    def dfs(box_index):
+        visited.add(box_index)
+        for key in boxes[box_index]:
+            if key < n and key not in visited:
+                dfs(key)
+
+    dfs(0)
+    return len(visited) == n
