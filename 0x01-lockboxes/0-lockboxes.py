85de2ac80ca4a90e7c90aa4b34bec6396c63fdf6
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-15 11:12:33 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+
+# You have n number of locked boxes in front of you.
+# Each box is numbered sequentially from 0 to n - 1
+# and each box may contain keys to the other boxes.
+
 def canUnlockAll(boxes):
+    # A function that checks if all boxes can be open
     n = len(boxes)
     visited = set()
 
     def dfs(box_index):
+        # A function that check keys and add each key to visited 
         visited.add(box_index)
         for key in boxes[box_index]:
             if key < n and key not in visited:
