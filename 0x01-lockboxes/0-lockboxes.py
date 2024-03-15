3e3585630fce4681b74bd2f23e01532aebe27405
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-15 11:24:44 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    # A function that checks if all boxes can be open
+    """ A function that checks if all boxes can be open """
     n = len(boxes)
     visited = set()
 
     def dfs(box_index):
-        # A function that check keys and add each key to visited 
+        """ A function that check keys and add each key to visited"""
         visited.add(box_index)
         for key in boxes[box_index]:
-            # loop through each box and check if key is visited or not
             if key < n and key not in visited:
                 dfs(key)
 
