252901890cc158c6b53cb0320b471913f7d8177e
Adetule-ayomide <adetuleayomide@gmail.com>
2024-04-12 01:52:14 +0100

    technical interview

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 import sys
 
+"""
+    The N queens puzzle is the challenge of placing N non-attacking
+    queens on an NxN chessboard. Write a program that solves the N
+    queens problem.
+"""
+
+
 def is_safe(board, row, col, n):
-    # Check if there is a queen in the same column
+    """Check if there is a queen in the same column"""
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-    # Check upper diagonal on left side
+    """Check upper diagonal on left side"""
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check upper diagonal on right side
+    """Check upper diagonal on right side"""
     for i, j in zip(range(row, -1, -1), range(col, n)):
         if board[i][j] == 1:
             return False
