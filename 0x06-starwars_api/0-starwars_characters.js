9a47a7e059fc93cbf10fa94f530c918a37b76a1a
Adetule-ayomide <adetuleayomide@gmail.com>
2024-04-18 23:28:36 +0000

    star wae Api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,41 +3,41 @@
 const axios = require('axios');
 
 function getMovieCharacters(movieId) {
-    return axios.get(`https://swapi.dev/api/films/${movieId}/`)
-        .then(response => {
-            return response.data.characters;
-        })
-        .catch(error => {
-            console.error(`Failed to retrieve data for movie ${movieId}:`, error);
-            return [];
-        });
+  return axios.get(`https://swapi.dev/api/films/${movieId}/`)
+    .then(response => {
+      return response.data.characters;
+    })
+    .catch(error => {
+      console.error(`Failed to retrieve data for movie ${movieId}:`, error);
+      return [];
+    });
 }
 
 function printMovieCharacters(movieId) {
-    getMovieCharacters(movieId)
-        .then(characters => {
-            if (characters.length > 0) {
-                console.log(`Characters in Star Wars Episode ${movieId}:`);
-                characters.forEach(characterUrl => {
-                    axios.get(characterUrl)
-                        .then(response => {
-                            console.log(response.data.name);
-                        })
-                        .catch(error => {
-                            console.error(`Failed to retrieve character data for ${characterUrl}:`, error);
-                        });
-                });
-            } else {
-                console.log("No characters found for this movie.");
-            }
-        });
+  getMovieCharacters(movieId)
+    .then(characters => {
+      if (characters.length > 0) {
+        console.log(`Characters in Star Wars Episode ${movieId}:`);
+        characters.forEach(characterUrl => {
+          axios.get(characterUrl)
+            .then(response => {
+              console.log(response.data.name);
+	    })
+            .catch(error => {
+              console.error(`Failed to retrieve character data for ${characterUrl}:`, error);
+	    });
+	});
+      } else {
+          console.log("No characters found for this movie.");
+      }
+    });
 }
 
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.error("Usage: node script.js <movie_id>");
-    process.exit(1);
+  console.error("Usage: node script.js <movie_id>");
+  process.exit(1);
 }
 
 printMovieCharacters(movieId);
