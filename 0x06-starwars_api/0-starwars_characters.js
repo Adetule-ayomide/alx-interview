a5eea8041db75ec6e60a742c01abffc28ed508c6
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-04-21 22:31:30 +0100

    star-war

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 const episodeNumber = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
 
 request(url, async (error, response, body) => {
-  if (error) {
-    reject(error);
-  } else if (response.statusCode !== 200) {
-    reject(new Error(`Failed to retrieve data for movie ${episodeNumber}`));
-  } else {
+  if (!error) {
     const movieData = JSON.parse(body);
     const characters = movieData.characters;
 
