33f9a4cb1ad5cad91ee5d1853586ace22b51753a
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-04-21 22:16:37 +0100

    star-war

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,49 +2,29 @@
 
 const request = require('request');
 
-function getMovieCharacters(movieId) {
-  return new Promise((resolve, reject) => {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Failed to retrieve data for movie ${movieId}`));
-      } else {
-        const movieData = JSON.parse(body);
-        resolve(movieData.characters);
-      }
-    });
-  });
-}
+const episodeNumber = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}`;
 
-function printMovieCharacters(movieId) {
-  getMovieCharacters(movieId)
-    .then(characters => {
-      console.log(`Characters in Star Wars Episode ${movieId}:`);
-      characters.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(`Failed to retrieve character data for ${characterUrl}`, error);
-          } else if (response.statusCode !== 200) {
-            console.error(`Failed to retrieve character data for ${characterUrl}. Status code: ${response.statusCode}`);
+request(url, async (error, response, body) => {
+  if (error) {
+    reject(error);
+  } else if (response.statusCode !== 200) {
+    reject(new Error(`Failed to retrieve data for movie ${episodeNumber}`));
+  } else {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    for (const character of characters) {
+      const promise = new Promise((resolve, reject) => {
+        request(character, (error, response, names) => {
+          if (!error) {
+            resolve(JSON.parse(names).name);
           } else {
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
+            reject(error);
           }
         });
       });
-    })
-    .catch(error => {
-      console.error(error.message);
-    });
-}
-
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error("Usage: node script.js <movie_id>");
-  process.exit(1);
-}
-
-printMovieCharacters(movieId);
+      console.log(await promise);
+    }
+  }
+});
